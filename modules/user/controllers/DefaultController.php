1654f7dc93ad76d4c0aadd0b0cfc4ca3530aa356
artemiev <mix@artemev.wing.igr.ru>
2016-09-27 18:10:35 +0300

    first commit

---
+++ modules/user/controllers/DefaultController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\modules\user\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Default controller for the `user` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Login action.
+     *
+     * @return string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return string
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionProfile()
+    {
+        $user = $this->findModel();
+        $model = new ProfileUpdateForm($user);
+
+        if ($model->load(Yii::$app->request->post()) && $model->update()) {
+            return $this->redirect(['index']);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+}
